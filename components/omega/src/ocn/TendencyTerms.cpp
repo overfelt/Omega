01a1261b5b4c764dcfc854ff4c34e2f28c0ab2cb
James Overfelt <jroverf@sandia.gov>
2026-06-30 13:42:30 -0600

    Add dumping of budget fields to output database.

--- components/omega/src/ocn/TendencyTerms.cpp
+++ components/omega/src/ocn/TendencyTerms.cpp
@@ -183,6 +183,46 @@ void TracerHorzAdvOnCell::init() {
          ActiveTracerHorizontalAdvectionTendency =
              Array3DReal("FCTActiveTracerHorizontalAdvectionTendency", NTracers,
                          NCellsAll, NVertLayers);
+         const std::string EdgeFieldName = "BudgetAdvectionEdgeFlux";
+         const Real FillValueReal        = -9.99e30;
+         const int NDims                 = 1;
+         std::vector<std::string> FluxDimNames(NDims, "NEdges");
+         auto BudgetAdvectionEdgeFlux = Field::create(
+             EdgeFieldName,                                      // field name
+             "Tracer FCT Horizontal Advection Edge Flux Budget", // long name or
+                                                                 // description
+             "",                                                 // units
+             "",                             // CF standard Name
+             0,                              // min valid value
+             std::numeric_limits<I4>::max(), // max valid value
+             FillValueReal,                  // scalar for undefined entries
+             NDims,                          // number of dimensions
+             FluxDimNames                    // dimension names
+         );
+         BudgetAdvectionEdgeFlux->attachData<Array3DReal>(
+             ActiveTracerHorizontalAdvectionEdgeFlux);
+         FieldGroup::addFieldToGroup(
+             ActiveTracerHorizontalAdvectionEdgeFlux.label(), Mesh->MeshName);
+         const std::string CellFieldName = "BudgetAdvectionTendency";
+         std::vector<std::string> TendDimNames(NDims, "NCells");
+         auto
+             BudgetAdvectionCellTend =
+                 Field::create(CellFieldName, // field name
+                               "Tracer FCT Horizontal Advection Cell Flux "
+                               "Tendency", // long name or description
+                               "",         // units
+                               "",         // CF standard Name
+                               0,          // min valid value
+                               std::numeric_limits<I4>::max(), // max valid
+                                                               // value
+                               FillValueReal, // scalar for undefined entries
+                               NDims,         // number of dimensions
+                               TendDimNames   // dimension names
+                 );
+         BudgetAdvectionCellTend->attachData<Array3DReal>(
+             ActiveTracerHorizontalAdvectionTendency);
+         FieldGroup::addFieldToGroup(
+             ActiveTracerHorizontalAdvectionTendency.label(), Mesh->MeshName);
       }
    }
 }
